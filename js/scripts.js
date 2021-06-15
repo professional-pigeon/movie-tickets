50c268ed6a8215fed20f94867fb5aabd351203c0
professional-pigeon <kyle.kayperez@gmail.com>
2021-06-15 15:00:57 -0700

    print a ticket

--- js/scripts.js
+++ js/scripts.js
@@ -15,11 +15,43 @@ function Ticket (movieInput, timeInput, ageInput, price) {
   this.price = price
 }
 
+
 let Titanic = new Movie("Titanic", "8:15", "12:00", "2:30", "6:00")
 
 //User-Interface Logic
 
-$(document).ready(function() {
+
+// function attachContactListeners() {
+//   $('ol#ticketReturn').on('click', 'li', function() {
+//     coolTicket.hide();
+//   })
+//   }
+
+$(document).ready(function() { 
+  
+  $("button#button1").click(function() {
+    const ticketPrice = 8;
+    let newTicketPrice = 0;
+    let movieName = $("#movieInput").val();
+    let time = $("#timeInput").val();
+    let age = $("#ageInput").val();
+      if (age >= 65) {
+        newTicketPrice = ticketPrice - 3;
+      } else if (time < 4) {
+        newTicketPrice = ticketPrice - 2;
+      } else {
+        newTicketPrice = 8;
+      }
+      
+    let coolTicket = new Ticket (movieName, time, age, newTicketPrice)
+    let ticketKeys = Object.keys(coolTicket);
+    let ticketString = "";
+    
+    ticketKeys.forEach(function(key){
+      ticketString = ticketString.concat(key + ": " + coolTicket[key] + " \n");
+      });
+    $('#ticketOutput').append("<li>" + ticketString + "</li>")
+  })
 
   let movieKeys = Object.keys(Titanic);
   let movieString = "";
@@ -27,11 +59,4 @@ $(document).ready(function() {
   movieString = movieString.concat(key + ": " + Titanic[key] + "\n"); 
   });
   $("ul").prepend("<li>" + movieString + "</li>")
-
-});
-
-let movieKeys = Object.keys(Titanic);
-let movieString = "";
-movieKeys.forEach(function(key) {
-  movieString = movieString.concat(key + ": " + Titanic[key] + "\n"); 
 });
